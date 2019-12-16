ddbaf40bb60fa1a62120a13443393cb128c5a212
Torrenti Sylvain <torrsyl@gmail.com>
2019-12-16 16:14:45 +0100

    ajout

--- ProjetTest/src/HelloWorld.java
+++ ProjetTest/src/HelloWorld.java
@@ -13,7 +13,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World !");
+		String nom = "Sylvain";
+		System.out.println("Hello World ! "+nom+" est le meilleur!");
 	}
 
 }
