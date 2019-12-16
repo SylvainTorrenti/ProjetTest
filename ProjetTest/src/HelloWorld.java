66c7820c255f674f744fb46cff9add596b560d9c
Torrenti Sylvain <torrsyl@gmail.com>
2019-12-16 09:28:58 +0100

    Ajout

---
+++ ProjetTest/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args argument
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+	}
+
+}
