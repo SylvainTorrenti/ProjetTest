7072c8cfeb08d2518102d1f3dcf4994ebe19fecf
Torrenti Sylvain <torrsyl@gmail.com>
2019-12-16 16:24:44 +0100

    ajout

--- ProjetTest/src/HelloWorld.java
+++ ProjetTest/src/HelloWorld.java
@@ -13,8 +13,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nom = "Sylvain";
-		System.out.println("Hello World ! "+nom+" est le meilleur!");
+		System.out.println("Hello World");
 	}
 
 }
