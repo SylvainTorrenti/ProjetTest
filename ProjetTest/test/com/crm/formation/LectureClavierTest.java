15e6b384c579a3cf2d4da74c5fe2c1db2e1b3468
Torrenti Sylvain <torrsyl@gmail.com>
2019-12-18 12:14:55 +0100

    ajout

---
+++ ProjetTest/test/com/crm/formation/LectureClavierTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+import java.util.Scanner;
+
+/**
+ * @author CRM
+ *
+ */
+public class LectureClavierTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("veuillez saisir un mot :");
+		String str = sc.nextLine();
+		System.out.println("Vous avez saisi : "+str);
+	}
+
+}
---
+++ ProjetTest/test/com/crm/formation/StringTest.java
@@ -0,0 +1,23 @@
+package com.crm.formation;
+
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class StringTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String texte;
+		texte = "hello";
+		System.out.println (texte);
+		
+	}
+
+}
