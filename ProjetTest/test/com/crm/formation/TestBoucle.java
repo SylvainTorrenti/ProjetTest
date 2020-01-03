3dcf2558338a516c29ee1b2940c40549ddfd3281
Torrenti Sylvain <torrsyl@gmail.com>
2020-01-03 10:09:09 +0100

    ajout

--- ProjetTest/test/com/crm/formation/TestBoucle.java
+++ ProjetTest/test/com/crm/formation/TestBoucle.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class TestBoucle {
 
 	public static void main(String[] args) {
-		//Une variable vide
+		
 		String prenom;
-		//On initialise celle-ci à O pour oui
 		char reponse = 'O';
-		//Notre objet Scanner, n'oubliez pas l'import de java.util.Scanner !
 		Scanner sc = new Scanner(System.in);
-		//Tant que la réponse donnée est égale à oui…
 		
 		while (reponse == 'O' && reponse != 'N')
 		{
