9914844e9b7fc7cdf9312c8998207b073e1cf8c3
Torrenti Sylvain <torrsyl@gmail.com>
2020-01-03 10:07:09 +0100

    ajout

--- ProjetTest/test/com/crm/formation/TestBoucle.java
+++ ProjetTest/test/com/crm/formation/TestBoucle.java
@@ -15,20 +15,13 @@ public class TestBoucle {
 		
 		while (reponse == 'O' && reponse != 'N')
 		{
-		  //On affiche une instruction
 		  System.out.println("Donnez un prénom : ");
-		  //On récupère le prénom saisi
 		  prenom = sc.nextLine();
-		  //On affiche notre phrase avec le prénom
 		  System.out.println("Bonjour " +prenom+ ", comment vas-tu ?");
-		  //On demande si la personne veut faire un autre essai
 		  System.out.println("Voulez-vous réessayer ? (O/N)");
-		  //On récupère la réponse de l'utilisateur
 		  reponse = sc.nextLine().charAt(0);
-		  //Tant que la réponse n'est pas O ou N, on repose la question
 		  while(reponse != 'O' && reponse != 'N')
 		  {
-			  //On demande si la personne veut faire un autre essai
 			    System.out.println("Voulez-vous réessayer ? (O/N)");
 			    reponse = sc.nextLine().charAt(0);
 			  }
