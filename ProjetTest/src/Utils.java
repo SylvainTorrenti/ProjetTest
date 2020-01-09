14bc1563d0018e597b959dd528f1e3fa864a52d7
Torrenti Sylvain <torrsyl@gmail.com>
2020-01-09 11:34:09 +0100

    ajout

---
+++ ProjetTest/src/Utils.java
@@ -0,0 +1,15 @@
+public class Utils {
+	public static final float PI = 3.14f;
+	public static final String COULEUR_BLEU = "bleu";
+	public static void afficherMessage(String texte) {
+		System.out.println(texte);
+	}
+	public static int somme(int a, int b) {
+		int s = a + b;
+		return s;
+	}
+	public static float surface(int rayon) {
+		float surface1 = rayon * rayon * PI;
+		return surface1;
+	}
+}
---
+++ ProjetTest/test/com/crm/formation/TestAppelFonction.java
@@ -0,0 +1,18 @@
+package com.crm.formation;
+
+import java.util.Scanner;
+
+public class TestAppelFonction {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double surface = 0;
+
+		System.out.println("Veuillez inserer le rayon:");
+		double rayon = sc.nextDouble();
+		surface = Math.pow(rayon, 2)*Math.PI;
+		System.out.println(surface);
+		
+	}
+
+}
