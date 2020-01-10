23e872ed0da1c0fdb9d4a9081ee5794c890d0011
Torrenti Sylvain <torrsyl@gmail.com>
2020-01-10 11:45:28 +0100

    ajout

---
+++ ProjetTest/src/GenerateurIdentifiants.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class GenerateurIdentifiants {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("veuillez saisir votre prénom :");
+		String prenom = sc.nextLine();
+		System.out.println("veuillez saisir votre nom :");
+		String nom = sc.nextLine();
+
+		prenom = prenom.substring(0, 1).toUpperCase();
+		nom = nom.toUpperCase();
+
+		System.out.println("Votre login sera: " + prenom + '.' + nom);
+
+		String nom1 = nom.substring(0, 2);
+		nom1 = nom1 + nom.substring(2).toLowerCase().replace('r', '@').replace('e', 'E').replace('t', 'T');
+
+		System.out.println("Votre mot de passe est: " + nom1 + nom1.length());
+	}
+
+}
--- ProjetTest/test/com/crm/formation/StringTest.java
+++ ProjetTest/test/com/crm/formation/StringTest.java
@@ -21,21 +21,31 @@ public class StringTest {
 //		System.out.println(texte);
 //		
 		String str = "Java c'est génial !!";
-//		
+//		Longueur d'une chaine
 //		System.out.println(str.length());
-		
+//		
+//		Mettre en maj
 		String str2 = str.toUpperCase();
 //		System.out.println(str2);
 		
+		
+//		supr blanc debut/fin
 		String str3 = str.trim();
 //		System.out.println(str3);
 		
+		
+//		remplacer caracteres
 		String str4 = str.replace('a', 'e');
 //		System.out.println(str4);
 		
-		String str5 = str.substring(0, 4);
-		System.out.println(str5);
 		
+//		extraction d'une sous chaine
+		String str5 = str.substring(0, 4);
+//		System.out.println(str5);
+//		
+//		Suppression des blancs
+		String str6 = str.replace(" ", "");
+		System.out.println(str6);
 		
 		
 		
