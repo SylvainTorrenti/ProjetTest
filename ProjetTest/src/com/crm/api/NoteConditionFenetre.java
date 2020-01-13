303ef022c8085d51d3bf972f04ba0d4019dabe15
Torrenti Sylvain <torrsyl@gmail.com>
2020-01-13 16:14:28 +0100

    ajout

--- ProjetTest/src/com/crm/api/NoteConditionFenetre.java
+++ ProjetTest/src/com/crm/api/NoteConditionFenetre.java
@@ -3,33 +3,82 @@ package com.crm.api;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+public class NoteConditionFenetre extends JFrame implements ActionListener {
 
-	public abstract class NoteConditionFenetre extends JFrame implements ActionListener {
+	private static JLabel label;
 
-		private JLabel label;
-		private JButton button;
-		String userWord = "";
+	String userWord = "";
 
-		public NoteConditionFenetre() {
-			setLayout(new FlowLayout());
-			setLocationRelativeTo(null);
+	public NoteConditionFenetre() {
+		setLayout(new FlowLayout());
+		setLocationRelativeTo(null);
+		label = new JLabel("Vous êtes ajourné!");
+		add(label);
 
-			label = new JLabel("Gabi est fort à lol!");
-			add(label);
+	}
 
-			button = new JButton("La vérité");
-			add(button);
-			button.addActionListener(this);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Inserez votre note:");
+		float note = sc.nextFloat();
+		sc.close();
+		if (note < 8) {
+			NoteConditionFenetre test = new NoteConditionFenetre();
+			test.setVisible(true);
+			test.setSize(300, 80);
+			test.setTitle("Resultat");
+
+		} else {
+			if (note < 10) {
+				NoteConditionFenetre test = new NoteConditionFenetre();
+				test.setVisible(true);
+				test.setSize(300, 80);
+				test.setTitle("Resultat");
+				label.setText("Admis");
+			} else {
+				if (note < 12) {
+					NoteConditionFenetre test = new NoteConditionFenetre();
+					test.setVisible(true);
+					test.setSize(300, 80);
+					test.setTitle("Resultat");
+					label.setText("Admis avec la mention B");
+				} else {
+					if (note < 14) {
+						NoteConditionFenetre test = new NoteConditionFenetre();
+						test.setVisible(true);
+						test.setSize(300, 80);
+						test.setTitle("Resultat");
+						label.setText("Vous êtes admis avec la mention AB");
+					} else {
+						if (note < 16) {
+							NoteConditionFenetre test = new NoteConditionFenetre();
+							test.setVisible(true);
+							test.setSize(300, 80);
+							test.setTitle("Resultat");
+							label.setText("Vous êtes admis avec la mention B");
+						} else {
+							if (note >= 16) {
+								NoteConditionFenetre test = new NoteConditionFenetre();
+								test.setVisible(true);
+								test.setSize(300, 80);
+								test.setTitle("Resultat");
+								label.setText("Vous êtes admis avec la mention TB");
+							}
+						}
+					}
+				}
+			}
 		}
-	
-	
-	
+	}
 
-		public static void main(String[] args) {
-		}
-		}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+}
